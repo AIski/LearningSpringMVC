2030682a43ebf4aba77b69bed1fda1536b5b9b26
Andrzej Szczepanski <androxtm@gmail.com>
2022-08-08 14:48:08 +0200

    added files

---
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,71 @@
+package com.luv2code.springdemo.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloWorldController {
+	
+	// controller method to show initial HRML form
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	// controller method to process the HTML form
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+
+	}
+	// new Controller method to read form data
+	
+	//add data to the model
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		
+		// read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
+		// convert the data to all caps
+		theName= theName.toUpperCase();
+		// create the message
+		
+		String result= "Yo! "+ theName;
+		//add message to the model
+		
+		model.addAttribute("message", result);
+		
+		
+		return "helloworld";
+		
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(
+			@RequestParam("studentName") 
+			String theName, Model model) {
+		
+	
+		// convert the data to all caps
+		theName= theName.toUpperCase();
+		// create the message
+		
+		String result= "Hey Mate from v3! "+ theName;
+		//add message to the model
+		
+		model.addAttribute("message", result);
+		
+		
+		return "helloworld";
+		
+	}
+	
+	
+	
+}
